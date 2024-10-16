a9b0ef9b4775d5b799f2c24daa15e7f10c0c4c6b
itsmedona <donashaji254@gmail.com>
2024-10-16 17:16:45 +0530

    Login ui set

--- lib/view/loginpage/loginpage.dart
+++ lib/view/loginpage/loginpage.dart
@@ -1,14 +1,243 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:login_animation/view/loginpage/widgets/animation.dart';
+import 'package:login_animation/view/loginpage/widgets/text.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  List<String> bgImgList = [
+    'assets/images/bg1.png',
+    'assets/images/bg2.png',
+    'assets/images/bg3.png',
+    'assets/images/bg4.png',
+    'assets/images/bg5.png',
+    'assets/images/bg6.png',
+    'assets/images/bg7.png',
+  ];
+  int selectedIndex = 0;
+  bool showOption = false;
+  bool passwordVisible = true;
+  TextEditingController passController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-      child: Text('LoginPage'),
+      floatingActionButton: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        height: 50,
+        width: double.infinity,
+        child: Row(
+          children: [
+            Expanded(
+              child: showOption
+                  ? ShowAnimation(
+                      delay: 100,
+                      child: SizedBox(
+                        height: 80,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: bgImgList.length,
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  selectedIndex = index;
+                                });
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 20),
+                                child: CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: selectedIndex == index
+                                      ? Colors.white
+                                      : Colors.transparent,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(1),
+                                    child: CircleAvatar(
+                                      radius: 25,
+                                      backgroundImage: AssetImage(
+                                        bgImgList[index],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    )
+                  : const SizedBox(),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            showOption
+                ? GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showOption = false;
+                      });
+                    },
+                    child: const Icon(
+                      Icons.close,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  )
+                : GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showOption = true;
+                      });
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: CircleAvatar(
+                          radius: 30,
+                          backgroundImage: AssetImage(
+                            bgImgList[selectedIndex],
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+          ],
+        ),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(bgImgList[selectedIndex]), fit: BoxFit.fill),
+        ),
+        alignment: Alignment.center,
+        child: Container(
+          height: 400,
+          width: double.infinity,
+          margin: EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white),
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.black.withOpacity(0.1),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Spacer(),
+                      Center(
+                          child:
+                              TextConst(text: "Email", weight: true, size: 30)),
+                      const Spacer(),
+                      TextConst(text: "Email"),
+                      Container(
+                        height: 35,
+                        decoration: const BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(color: Colors.white))),
+                        child: TextFormField(
+                          style: const TextStyle(color: Colors.white),
+                          decoration: const InputDecoration(
+                            suffixIcon: Icon(
+                              Icons.mail,
+                              color: Colors.white,
+                            ),
+                            fillColor: Colors.white,
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      const Spacer(),
+                      TextConst(
+                        text: "Password",
+                      ),
+                      Container(
+                        height: 35,
+                        decoration: const BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(color: Colors.white))),
+                        child: TextFormField(
+                          controller: passController,
+                          obscureText: passwordVisible,
+                          style: const TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              icon: passwordVisible == false
+                                  ? Icon(
+                                      Icons.visibility,
+                                      color: Colors.white,
+                                    )
+                                  : Icon(
+                                      Icons.visibility_off_outlined,
+                                      color: Colors.white,
+                                    ),
+                              onPressed: () {
+                                setState(() {
+                                  passwordVisible = !passwordVisible;
+                                });
+                              },
+                            ),
+                            fillColor: Colors.white,
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      const Spacer(),
+                      Row(
+                        children: [
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                              child: TextConst(
+                            text: "FORGET PASSWORD",
+                            size: 12,
+                            weight: true,
+                          ))
+                        ],
+                      ),
+                      const Spacer(),
+                      Container(
+                        height: 40,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30)),
+                        alignment: Alignment.center,
+                        child: TextConst(
+                          text: "Log In",
+                          color: Colors.black,
+                        ),
+                      ),
+                      const Spacer(),
+                      Center(
+                          child: TextConst(
+                        text: "Don't have a account REGISTER",
+                        size: 12,
+                        weight: true,
+                      )),
+                      const Spacer(),
+                    ],
+                  ),
+                )),
+          ),
+        ),
       ),
     );
   }
